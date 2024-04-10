97320f6245afc164e8f8f1844e516a8a20daf9c4
Opilane TTHK <opilane@tthk.ee>
2024-04-10 10:10:23 +0300

    2

---
+++ FreeNetflixMaui/FreeNetflixMaui/Services/TmdbService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Services
+{
+    public partial class TmdbService
+    {
+        private const string ApiKey = "738b6d8d99b51339705b1037c981a488"; // generate it from tmdb website
+        public const string TmdbHttpClientName = "TmdbClient";
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public TmdbService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        private HttpClient HttpClient => _httpClientFactory.CreateClient(TmdbHttpClientName);
+
+
+
+    }
+}
